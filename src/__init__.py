493fe06db59d75142044a32b523fe3ffe803bafb
Till Schallau <ts@netzlab.de>
2020-04-25 11:25:03 +0200

    Add return values to functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,15 @@
 from array import *
 from numpy import random
 
+
 def sudoku_correct(sudoku):
+    correct = True
     for row in range(8):
         for col in range(8):
             value = sudoku[row][col]
-            check_value(row, col, sudoku, value)
+            correct &= check_value(row, col, sudoku, value)
+    return correct
+
 
 def check_value(row, col, sudoku, value):
     for index in range(8):
@@ -13,10 +17,12 @@ def check_value(row, col, sudoku, value):
             other_val = sudoku[row][index]
             if other_val == value:
                 print(f"Value not correct. Row: {row}, Column: {col}, Value: {value}")
+                return False
+    return True
 
 
 T = random.choice(list(range(0, 9)), size=(9, 9))
 
 print(T)
 
-sudoku_correct(T)
+print(f"Correct? {sudoku_correct(T)}")
